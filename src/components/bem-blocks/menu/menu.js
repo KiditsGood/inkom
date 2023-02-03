3ac472821b4480e174237494348717734a8db39f
KiditsGood <71182254+KiditsGood@users.noreply.github.com>
2023-02-03 16:00:42 +0300

    index done

--- src/components/bem-blocks/menu/menu.js
+++ src/components/bem-blocks/menu/menu.js
@@ -1,7 +1,7 @@
 const catalogMobile = document.querySelector('button.menu__item')
 const subSelector = document.querySelector('.sub')
 const subButton = document.querySelector('.nav > .sub__head')
-const subBack = document.querySelector('.sub__back')
+const subBack = document.querySelector('.sub__list > .sub__back')
 
 function mobileCatalogOpen() {
     catalogMobile.classList.toggle('active')
--- src/components/bem-blocks/minicatalog/miniCatalog.js
+++ src/components/bem-blocks/minicatalog/miniCatalog.js
@@ -2,11 +2,53 @@ const subHidden = document.querySelector('.sub__hidden')
 const subHiddenButton = document.querySelector('.sub__hidden-open')
 const bodySelector = document.querySelector('body')
 const subHead = document.querySelector('.sub > .sub__head')
+const hiddenButton = document.querySelectorAll('button.sub__list-item')
+const prefixSelector = document.querySelectorAll('.prefix')
+const prefixBack = document.querySelectorAll('.prefix > .sub__back')
 
 function subOpen() {
     subHidden.classList.toggle('active')
+    hiddenButton.forEach(el => {
+        el.classList.remove('active')
+    })
+    prefixSelector.forEach(el => {
+        el.classList.remove('active')
+    })
+}
+
+function hiddenOpen(hiddenElem) {
+    if (hiddenElem.classList.contains('active')){
+        hiddenElem.classList.remove('active')
+        hiddenElem.nextElementSibling.classList.remove('active')
+    }
+    else {
+        hiddenButton.forEach(el => {
+            el.classList.remove('active')
+        })
+        prefixSelector.forEach(el => {
+            el.classList.remove('active')
+        })
+
+        hiddenElem.classList.add('active')
+        hiddenElem.nextElementSibling.classList.add('active')
+
+        if(window.innerWidth < 1000){
+            subSelector.classList.remove('active')
+        }
+    }
 }
 
 subHiddenButton.addEventListener('click', subOpen)
 subHead.addEventListener('click', subOpen)
-
+hiddenButton.forEach(el => {
+    el.addEventListener('click', function () {
+        hiddenOpen(el)
+    })
+})
+prefixBack.forEach(el => {
+    el.addEventListener('click', function () {
+        el.parentElement.classList.remove('active')
+        subSelector.classList.add('active')
+        el.parentElement.previousElementSibling.classList.remove('active')
+    })
+})
